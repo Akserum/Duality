1d0b888073880d04702b3ae91c0aa9a5947ee30e
Akserum <akihana@live.be>
2022-01-28 22:07:52 +0100

    navmesh ok

---
+++ Assets/Scripts/PoubScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoubScript : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        print("enters trigger enter");
+    }
+}
